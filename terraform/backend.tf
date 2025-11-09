009369f71308d257fc391e46a06659846bb24a36
Volodymyr Melnyk <vovazb1232@gmail.com>
2025-11-10 01:35:50 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "itm-25"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
